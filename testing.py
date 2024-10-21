fb15436714601fe5f6fa597705072116ca0ee418
fabioBaps <fabiox2509@gmail.com>
2024-10-21 12:07:09 -0300

    deleting unused things pt2

--- testing.py
+++ testing.py
@@ -8,9 +8,9 @@ from objectClasses import CircleObject, CircleStaticObject
 ################## INITIALIZATION AND START VALUES
 pygame.init(); WIDTH, HEIGHT = 800, 600; window = pygame.display.set_mode((WIDTH, HEIGHT)); pygame.display.set_caption("Swarm robots")
 # x,y = [random.randint(60, WIDTH-60) for _ in range(3)], [random.randint(60, HEIGHT-60) for _ in range(3)]; radius = 30; color = (0, 0, 0); vel_x = 0; vel_y = 0; mass = 0.1
-x = [100, 150, 100]; y = [100, 100, 150]; radius = 30; color = (0, 0, 0); vel_x = 0; vel_y = 0; mass = 0.1
+x = [100, 150, 100]; y = [100, 100, 150]; radius = 5; color = (0, 0, 0); vel_x = 0; vel_y = 0; mass = 0.1
 objects = [CircleObject(i, x[i], y[i], radius, color, vel_x, vel_y, mass) for i in range(len(x))]
-xS = WIDTH//2; yS = HEIGHT//2; radiusS = 50; colorS = (255, 0, 0); mass = 0.1
+xS = WIDTH//2; yS = HEIGHT//2; radiusS = 20; colorS = (255, 0, 0); mass = 0.1
 static_object = CircleStaticObject(0, xS, yS, radiusS, colorS, mass, objects)
 
 def handle_events():
@@ -20,7 +20,7 @@ def handle_events():
             return True
     return False
 
-def objectsPositions(objects, static_object, dest, action = None):
+def objectsPositions(objects, static_object, dest):
     positions = []
     dx, dy = dest[0] - static_object.x, dest[1] - static_object.y
     signX = sign(dx)
@@ -33,7 +33,7 @@ def objectsPositions(objects, static_object, dest, action = None):
         alpha = 0 # 0 or 180 degrees
     else:
         alpha =  arctan(dy/dx)
-    sumR = static_object.radius + objects[0].radius + (10 if action else 0) # because we are assuming that every object has the same radius
+    sumR = static_object.radius + objects[0].radius # because we are assuming that every object has the same radius
     sumRx2Sq = 2*sumR**2
     if len(objects) % 2 != 0:
         positions.append((static_object.x + signX * cos(alpha + pi) * sumR, static_object.y + signX * sin(alpha + pi) * sumR))
@@ -64,8 +64,8 @@ def objectsTo(objects, positions):
 ################## PROBLEM 1: STATIC OBJECT NEED TO BE MOVED TO A SPECIFIC POSITION
 # dest = (WIDTH - 100, 100)
 # dest = (100, 100)
-# dest = (100, HEIGHT - 100)
-dest = (WIDTH - 100, HEIGHT - 100)
+dest = (100, HEIGHT - 100)
+# dest = (WIDTH - 100, HEIGHT - 100)
 # dest = (WIDTH // 2, 100)
 dmCoords = objectsPositions(objects, static_object, dest)
 num_objects = len(dmCoords)
@@ -86,25 +86,22 @@ while True:
         # destVel_y = (dest[1] - static_object.y) / 1000
         # static_object.vel_x += destVel_x
         # static_object.vel_y += destVel_y
-        static_object.RKMove(dest, 20)
+        static_object.RKMove(dest, 16)
         for obj in objects:
-            obj.RKMove(pairsPlusVec[obj.id], 20)
+            obj.RKMove(pairsPlusVec[obj.id], 16)
             # obj.vel_x += destVel_x
             # obj.vel_y += destVel_y
             
     else:
-        # index = dmList.index(True)
-        # dmList[index] = objects[index].directMove(*pairs[index], objects)
-        for obj in objects:
-            dmList[obj.id] = obj.directMove(*pairs[obj.id], objects)
+        index = dmList.index(True)
+        dmList[index] = objects[index].RKMove(pairs[index],16)
+        # for obj in objects:
+        #     dmList[obj.id] = obj.RKMove(pairs[obj.id], 16)
     
     static_object.move(WIDTH, HEIGHT)
     static_object.draw(window)
     
     for ind in range(num_objects):
-        # if dmList[ind]:
-        #     dmList[ind] = objects[ind].directMove(*dmCoords[ind], objects)
-        # obj.pointMove(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
         objects[ind].move(WIDTH, HEIGHT)
         objects[ind].draw(window)
     pygame.display.update()
