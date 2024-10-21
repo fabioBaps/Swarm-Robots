26b7193b03ca9a4c29783421ceef3a90b0c373dd
fabioBaps <fabiox2509@gmail.com>
2024-10-21 11:45:03 -0300

    changing tf to 0.01

--- RK44Functions.py
+++ RK44Functions.py
@@ -1,17 +1,17 @@
 ################## IMPORTS
 from numpy import array
 
-def phi(y, dest, dt, f):
+def phi(y, dest, h, f):
     k1 = f(y, dest)
-    k2 = f(y + dt/2*k1, dest)
-    k3 = f(y + dt/2*k2, dest)
-    k4 = f(y + dt*k3, dest)
+    k2 = f(y + (h/2)*k1, dest)
+    k3 = f(y + (h/2)*k2, dest)
+    k4 = f(y + h*k3, dest)
     return 1/6*(k1 + 2*k2 + 2*k3 + k4)
 
 ################## RUNGE KUTTA 4TH ORDER METHOD
 def oneStepMethod(y0, dest, n, f):
     y_n = [array(y0)]
-    h = 1/n
+    h = 0.01/n
     for _ in range(n):
         y_n.append(y_n[-1] + h*phi(y_n[-1], dest, h, f))
     return y_n[-1]
