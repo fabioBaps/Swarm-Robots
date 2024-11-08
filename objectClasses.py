1d232ceaae67c519624c410df004e9aca5be4e01
fabioBaps <fabiox2509@gmail.com>
2024-11-08 16:37:32 -0300

    2 dimensions

--- objectClasses.py
+++ objectClasses.py
@@ -1,120 +1,100 @@
 ################## IMPORTS
 import pygame
 import math
-from numpy import sign, array, cos, sin
+from numpy import array, cos, sin
 from RK44Functions import oneStepMethod
 
 ################## MOVING OBJECTS CLASSES                
 class CircleObject:
-    def __init__(self, id, x, y, radius, color, vel_x, vel_y, mass):
+    def __init__(self, id, coord, vel, radius, color, mass):
         self.id = id
-        self.x = x
-        self.y = y
+        self.coord = coord
+        self.vel = vel
         self.radius = radius
         self.color = color
-        self.vel_x = vel_x
-        self.vel_y = vel_y
         self.m = mass
     
     def draw(self, window):
-        pygame.draw.circle(window, self.color, (self.x, self.y), self.radius)
+        pygame.draw.circle(window, self.color, self.coord, self.radius)
 
     def border(self, WIDTH, HEIGHT):
-        if self.x <= self.radius:
-            self.x = self.radius
-        elif self.x + self.radius >= WIDTH:
-            self.x = WIDTH - self.radius
-        if self.y <= self.radius:
-            self.y = self.radius
-        elif self.y + self.radius >= HEIGHT:
-            self.y = HEIGHT - self.radius
+        if self.coord[0] <= self.radius:
+            self.coord[0] = self.radius
+        elif self.coord[0] + self.radius >= WIDTH:
+            self.coord[0] = WIDTH - self.radius
+        if self.coord[1] <= self.radius:
+            self.coord[1] = self.radius
+        elif self.coord[1] + self.radius >= HEIGHT:
+            self.coord[1] = HEIGHT - self.radius
             
-    def fx(self, x, dest, angle):
-        f0 = x[1]
-        # f1 = (1/self.m) * (dest - x[0] - 0.8 * x[1])
-        f1 = (1/self.m) * (cos(angle) * sign(dest - x[0]) - 0.8 * x[1]) # limit velocity to 10 units
-        return array([f0, f1])
-    
-    def fy(self, x, dest, angle):
-        f0 = x[1]
-        # f1 = (1/self.m) * (dest - x[0] - 0.8 * x[1])
-        f1 = (1/self.m) * (sin(angle) * sign(dest - x[0]) - 0.8 * x[1]) # limit velocity to 10 units
+    def f(self, coordsVels, dest):
+        f0 = array(coordsVels[1])
+        # dest[2] is the angle between the x-axis and the line that connects the object to the destination
+        f1 = (1/self.m) * (array([cos(dest[2]), sin(dest[2])]) - 0.8 * f0)
         return array([f0, f1])
                         
     def EulerMove(self, dest, n):
-        if math.hypot(dest[0] - self.x, dest[1] - self.y) <= 1:
+        if math.hypot(*(dest[:2] - self.coord)) <= 1:
             return False
         else:
             h = 1/n
             for _ in range(n):
-                fx, fvx = self.f([self.x, self.vel_x], dest[0])
-                self.x += h * fx; self.vel_x += h * fvx
-                fy, fvy = self.f([self.y, self.vel_y], dest[1])
-                self.y += h * fy; self.vel_y += h * fvy
-            return [self.x, self.y]
+                f, fv = self.f([self.coord, self.vel], dest)
+                self.coord = self.coord + h * f; self.vel = self.vel + h * fv
+
+            return True
 
     def RKMove(self, dest, n):
-        if math.hypot(dest[0] - self.x, dest[1] - self.y) <= 1:
+        if math.hypot(*(dest[:2] - self.coord)) <= 1:
+            self.vel = array([0, 0])
             return False
         else:
-            self.x, self.vel_x = oneStepMethod([self.x, self.vel_x], dest[0], n, self.fx, angle=dest[2])
-            self.y, self.vel_y = oneStepMethod([self.y, self.vel_y], dest[1], n, self.fy, angle=dest[2])
+            self.coord, self.vel = oneStepMethod([self.coord, self.vel], dest, n, self.f)
             return True
 
 ################## STATIC OBJECTS CLASSES
 class CircleStaticObject:
-    def __init__(self, id, x, y, radius, color, mass):
+    def __init__(self, id, coord, radius, color, mass):
         self.id = id
-        self.x = x
-        self.y = y
+        self.coord = coord
         self.radius = radius
         self.color = color
-        self.vel_x = 0
-        self.vel_y = 0
+        self.vel = [0, 0]
         self.m = mass
         self.done = True
 
     def draw(self, window):
-        pygame.draw.circle(window, self.color, (self.x, self.y), self.radius)
-        
-    def fx(self, x, dest, angle):
-        f0 = x[1]
-        # f1 = (1/self.m) * (dest - x[0] - 0.8 * x[1])
-        f1 = (1/self.m) * 10 * (cos(angle) * sign(dest - x[0]) - 0.8 * x[1])
-        return array([f0, f1])
+        pygame.draw.circle(window, self.color, self.coord, self.radius)
     
-    def fy(self, x, dest, angle):
-        f0 = x[1]
-        # f1 = (1/self.m) * (dest - x[0] - 0.8 * x[1])
-        f1 = (1/self.m) * 10 * (sin(angle) * sign(dest - x[0]) - 0.8 * x[1])
+    def f(self, coordsVels, dest):
+        f0 = array(coordsVels[1])
+        f1 = (1/self.m) * 10 * (array([cos(dest[2]), sin(dest[2])]) - 0.8 * f0)
         return array([f0, f1])
 
     def border(self, WIDTH, HEIGHT):
-        if self.x <= self.radius:
-            self.x = self.radius
-        elif self.x + self.radius >= WIDTH:
-            self.x = WIDTH - self.radius
-        if self.y <= self.radius:
-            self.y = self.radius
-        elif self.y + self.radius >= HEIGHT:
-            self.y = HEIGHT - self.radius
+        if self.coord[0] <= self.radius:
+            self.coord[0] = self.radius
+        elif self.coord[0] + self.radius >= WIDTH:
+            self.coord[0] = WIDTH - self.radius
+        if self.coord[1] <= self.radius:
+            self.coord[1] = self.radius
+        elif self.coord[1] + self.radius >= HEIGHT:
+            self.coord[1] = HEIGHT - self.radius
             
     def EulerMove(self, dest, n):
-        if math.hypot(dest[0] - self.x, dest[1] - self.y) <= 1:
+        if math.hypot(*(dest[:2] - self.coord)) <= 1:
             self.done = False # static object has reached its destination, end simulation
             return
         else:
             h = 1/n
             for _ in range(n):
-                fx, fvx = self.f([self.x, self.vel_x], dest[0])
-                self.x += h * fx; self.vel_x += h * fvx
-                fy, fvy = self.f([self.y, self.vel_y], dest[1])
-                self.y += h * fy; self.vel_y += h * fvy
+                f, fv = self.f([self.coord, self.vel], dest)
+                self.coord = self.coord + h * f; self.vel = self.vel + h * fv
 
     def RKMove(self, dest, n):
-        if math.hypot(dest[0] - self.x, dest[1] - self.y) <= 1:
+        if math.hypot(*(dest[:2] - self.coord)) <= 1:
+            self.vel = array([0, 0])
             self.done = False
             return
         else:
-            self.x, self.vel_x = oneStepMethod([self.x, self.vel_x], dest[0], n, self.fx, angle=dest[2])
-            self.y, self.vel_y = oneStepMethod([self.y, self.vel_y], dest[1], n, self.fy, angle=dest[2])
+            self.coord, self.vel = oneStepMethod([self.coord, self.vel], dest, n, self.f)
--- testing.py
+++ testing.py
@@ -3,23 +3,22 @@ import pygame
 # from pygame_screen_record import ScreenRecorder
 import random
 import math
-from numpy import pi, sin, cos, arctan2, arccos, array
+from numpy import pi, sin, cos, arctan2, arccos, array, append, flip
 from objectClasses import CircleObject, CircleStaticObject
-import matplotlib.pyplot as plt
 
 ################## INITIALIZATION AND START VALUES
-for h in [16, 32, 64]:
-    xn, xpn, yn, ypn = [], [], [], []
-    pygame.init(); WIDTH, HEIGHT = 800, 600; window = pygame.display.set_mode((WIDTH, HEIGHT)); pygame.display.set_caption("Swarm robots"); #recorder = ScreenRecorder(60, window); recorder.start_rec()
+pygame.init(); WIDTH, HEIGHT = 800, 600; window = pygame.display.set_mode((WIDTH, HEIGHT)); pygame.display.set_caption("Swarm robots"); #recorder = ScreenRecorder(60, window); recorder.start_rec()
+for h in [32]:
     radius = 5; oX = ['left', 'middle', 'right'][0]; oY = ['top', 'middle', 'bottom'][0]; num_objects = 1
     # x = [random.randint(*{'left': (radius, WIDTH//2 - radius - 2), 'middle': (radius, WIDTH//2 - radius - 2), 'right': (WIDTH//2 + radius + 2, WIDTH - radius)}[oX]) for _ in range(num_objects)]
     # y = [random.randint(*{'top': (radius, HEIGHT//2 - radius - 2), 'middle': (radius, HEIGHT//2 - radius - 2), 'bottom': (HEIGHT//2 + radius + 2, HEIGHT - radius)}[oY]) for _ in range(num_objects)]
     x, y = [100], [100]
     color = (0, 0, 0); vel_x = 0; vel_y = 0; mass = 0.1
-    # x = [100, 150, 100]; y = [100, 100, 150];color = (0, 0, 0); vel_x = 0; vel_y = 0; mass = 0.1
-    objects = [CircleObject(i, x[i], y[i], radius, color, vel_x, vel_y, mass) for i in range(len(x))]
+    coords = [array([x[i], y[i]]) for i in range(num_objects)]; vels = [array([vel_x, vel_y]) for _ in range(num_objects)]
+    objects = [CircleObject(i, coords[i], vels[i], radius, color, mass) for i in range(len(x))]
     xS = WIDTH//2; yS = HEIGHT//2; radiusS = 20; colorS = (255, 0, 0); mass = 1
-    static_object = CircleStaticObject(0, xS, yS, radiusS, colorS, mass)
+    coordS = array([xS, yS])
+    static_object = CircleStaticObject(0, coordS, radiusS, colorS, mass)
 
     def handle_events():
         for event in pygame.event.get():
@@ -30,52 +29,52 @@ for h in [16, 32, 64]:
 
     def objectsPositions(objects, static_object, dest):
         positions = []
-        dx, dy = static_object.x - dest[0], static_object.y - dest[1]
+        dx, dy = static_object.coord - dest[:2]
         alpha = arctan2(dy, dx) # returns the angle between the x-axis and the line that connects the static object to the destination + pi
         sumR = static_object.radius + objects[0].radius # because we are assuming that every object has the same radius
         sumRx2Sq = 2*sumR**2
         if len(objects) % 2 != 0:
-            positions.append([static_object.x + cos(alpha) * sumR, static_object.y + sin(alpha) * sumR])
+            positions.append(static_object.coord + array([cos(alpha), sin(alpha)]) * sumR)
             for l in range(1, len(objects)):
                 bw = (2 * arccos((sumRx2Sq - objects[0].radius**2)/(sumRx2Sq))) * (1 if l % 2 == 0 else -1) # if l is even, the angle is negative (bw for between)
                 angleAdd = ((l-1)//2 + 1) * bw
                 if abs(angleAdd + bw/2) > pi/2:
                     return positions
-                positions.append([static_object.x + cos(alpha + angleAdd) * sumR, static_object.y + sin(alpha + angleAdd) * sumR])
+                positions.append(static_object.coord + array([cos(alpha + angleAdd), sin(alpha + angleAdd)]) * sumR)
         else:
             for l in range(len(objects)):
                 bw = (arccos((sumRx2Sq - objects[0].radius**2)/(sumRx2Sq))) * (1 if l % 2 == 0 else -1) # if l is even, the angle is negative (bw for between)
                 angleAdd = ((l//2)*2 + 1) * bw
                 if abs(angleAdd + bw) > pi/2:
                     return positions
-                positions.append([static_object.x + cos(alpha + angleAdd) * sumR, static_object.y + sin(alpha + angleAdd) * sumR])
+                positions.append(static_object.coord + array([cos(alpha + angleAdd), sin(alpha + angleAdd)]) * sumR)                
         return positions
 
     def objectsTo(objects, positions):
         pairs = {}
         candidates = list(range(len(objects)))
         for pos in positions:
-            closeIndex = array([math.hypot(pos[0] - objects[n].x, pos[1] - objects[n].y) for n in candidates]).argmin() # returns the index of the closest object to pos
-            pos.append(arctan2(pos[1] - objects[candidates[closeIndex]].y, pos[0] - objects[candidates[closeIndex]].x))
+            closeIndex = array([math.hypot(pos[0] - objects[n].coord[0], pos[1] - objects[n].coord[1]) for n in candidates]).argmin() # returns the index of the closest object to pos
+            pos = append(pos, arctan2(*flip(pos - objects[candidates[closeIndex]].coord, 0))) # flip the subtraction to get dy, dx
             pairs[candidates[closeIndex]] = pos
             candidates.pop(closeIndex)
         return pairs
 
     ################## PROBLEM 1: STATIC OBJECT NEED TO BE MOVED TO A SPECIFIC POSITION
     dest = [{'left': WIDTH - 100, 'middle': WIDTH//2, 'right': 100}[oX], {'top': HEIGHT - 100, 'middle': HEIGHT//2, 'bottom': 100}[oY]]
-    dest.append(arctan2(dest[1] - static_object.y, dest[0] - static_object.x))
+    dest.append(arctan2(*flip(dest[:2] - static_object.coord, 0)))
     dmCoords = objectsPositions(objects, static_object, dest)
     num_objects = len(dmCoords)
     objects = objects[:num_objects]
-    pairs = objectsTo(objects, dmCoords) 
+    pairs = objectsTo(objects, dmCoords)
     dmList = [True]*num_objects
 
     ################## MAIN LOOP
     clock = pygame.time.Clock()
-    vec = array(dest) - array([static_object.x, static_object.y, dest[2]])
+    vec = array(dest) - array([*static_object.coord, dest[2]])
     pairsPlusVec = [array(pairs[obj.id]) + vec for obj in objects]
     for obj in objects:
-        pairsPlusVec[obj.id][2] = arctan2(pairsPlusVec[obj.id][1] - pairs[obj.id][1], pairsPlusVec[obj.id][0] - pairs[obj.id][0])
+        pairsPlusVec[obj.id][2] = arctan2(*flip(pairsPlusVec[obj.id][:2] - pairs[obj.id][:2], 0))
     while static_object.done:
         if handle_events():
             break
@@ -104,40 +103,7 @@ for h in [16, 32, 64]:
         pygame.draw.line(window, (255, 0, 0), pairs[0][:2], dest[:2], 2)
         pygame.display.update()
         clock.tick(60)
-        xn.append(objects[0].x)
-        xpn.append(objects[0].vel_x)
-        yn.append(objects[0].y)
-        ypn.append(objects[0].vel_y)
-    fig, axs = plt.subplots(2, 2, figsize=(10, 8))
-    fig.suptitle(f'Approximations with h = {h}')
-
-    axs[0, 0].plot([i for i in range(len(xn))], xn)
-    axs[0, 0].set_title('x')
-    axs[0, 0].grid(True)
-
-    axs[0, 1].plot([i for i in range(len(xpn))], xpn)
-    axs[0, 1].set_title('xpn')
-    axs[0, 1].grid(True)
-
-    axs[1, 0].plot([i for i in range(len(yn))], yn)
-    axs[1, 0].set_title('yn')
-    axs[1, 0].grid(True)
-
-    axs[1, 1].plot([i for i in range(len(ypn))], ypn)
-    axs[1, 1].set_title('ypn')
-    axs[1, 1].grid(True)
-
-    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-    
-#     print('----------------------------------')
-# print("%5s & %9s & %9s & %9s \\\\" % ('n', 'h', 'e', 'p'))
-# for h, j in [(16,0), (32,1), (64,2), (128,3)]:
-#     p = math.log(abs((xn[j]-xn[j+1])/(xn[j+1]-xn[j+2])))/math.log(2)
-#     print("%5d & %9.3e & %9.3e & %9.3e \\\\" % (h, 1/h, abs((xn[j+1]-xn[j+2])/(2**p - 1)), p))
 
 # recorder.stop_rec()
 # recorder.save_recording('tcc/moveRK.mp4')
 pygame.quit()
-
-
-plt.show()
